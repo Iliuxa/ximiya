9a59258259226e0b6dbff9a5da868a2c886d70db
Олег <oleg.oj17@yandex.ru>
2024-04-06 21:22:27 +0300

    Добавьте файлы проекта.

---
+++ ximiya/x.c
@@ -0,0 +1,38 @@
+#include <stdio.h>
+
+int main(void)
+{
+    char str;
+    int mode[6];
+
+
+
+
+    char* filename = "D:\\Internet Exploer\\ximiya\\x64\\Debug\\VXMEPRSH.txt";
+
+    FILE* fp = fopen(filename, "r");
+    if (fp)
+    {
+
+        for (int i = 0; i < 6; i++) {
+            fscanf_s(fp, "%u", &mode[i]);
+            fgets(str, 80, fp);
+        }
+
+        int a = 0;
+       
+
+
+
+
+        fclose(fp);
+    }
+
+
+
+
+
+
+
+    return 0;
+}
