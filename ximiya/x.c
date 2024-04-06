9c9a29b590380134a460992f82729be5ee6144bf
Олег <oleg.oj17@yandex.ru>
2024-04-06 22:31:21 +0300

    Получение переменных

--- ximiya/x.c
+++ ximiya/x.c
@@ -2,32 +2,91 @@
 
 int main(void)
 {
-    char str;
-    int mode[6];
+	char line[80 + 1];
+
+	int mode[6];
+	float params[3];
+	float var_1[8];
+	float var_ch_1[49];
+	float var_2[8];
+	float var_ch_2[49];
+	float var_3[8];
+	float var_4[8];
+	float var_5[6];
+	float var_6[4];
+	float var_7[8];
+
+
+	char* filename = "D:\\Internet Exploer\\ximiya\\x64\\Debug\\VXMEPRSH.txt";
+	FILE* fp = fopen(filename, "r");
+	if (fp == NULL) {
+		printf("Ошибка открытия файла.\n");
+		return 1;
+	}
+
+
+	for (int i = 0; i < 6; i++) {
+		fscanf_s(fp, "%u", &mode[i]);
+		while (fgetc(fp) != '\n');
+	}
+
+	for (int i = 0; i < 3; i++) {
+		fscanf_s(fp, "%f", &params[i]);
+		while (fgetc(fp) != '\n');
+	}
+
+	for (int i = 0; i < 8; i++) {
+		fscanf_s(fp, "%f", &var_1[i]);
+		while (fgetc(fp) != '\n');
+	}
+
+	for (int i = 0; i < 49; i++) {
+		fscanf_s(fp, "%f", &var_ch_1[i]);
+		while (fgetc(fp) != '\n');
+	}
+
+	for (int i = 0; i < 8; i++) {
+		fscanf_s(fp, "%f", &var_2[i]);
+		while (fgetc(fp) != '\n');
+	}
+
+	for (int i = 0; i < 49; i++) {
+		fscanf_s(fp, "%f", &var_ch_2[i]);
+		while (fgetc(fp) != '\n');
+	}
+
+	for (int i = 0; i < 8; i++) {
+		fscanf_s(fp, "%f", &var_3[i]);
+		while (fgetc(fp) != '\n');
+	}
+
+	for (int i = 0; i < 8; i++) {
+		fscanf_s(fp, "%f", &var_4[i]);
+		while (fgetc(fp) != '\n');
+	}
+
+	for (int i = 0; i < 6; i++) {
+		fscanf_s(fp, "%f", &var_5[i]);
+		while (fgetc(fp) != '\n');
+	}
+
+	for (int i = 0; i < 4; i++) {
+		fscanf_s(fp, "%f", &var_6[i]);
+		while (fgetc(fp) != '\n');
+	}
+
+	for (int i = 0; i < 8; i++) {
+		fscanf_s(fp, "%f", &var_7[i]);
+		while (fgetc(fp) != '\n');
+	}
 
 
 
 
-    char* filename = "D:\\Internet Exploer\\ximiya\\x64\\Debug\\VXMEPRSH.txt";
-
-    FILE* fp = fopen(filename, "r");
-    if (fp)
-    {
-
-        for (int i = 0; i < 6; i++) {
-            fscanf_s(fp, "%u", &mode[i]);
-            fgets(str, 80, fp);
-        }
+	int a = 0;
 
 
-        int a = 0;
-       
-
-
-         
-
-        fclose(fp);
-    }
+	fclose(fp);
 
 
 
@@ -35,5 +94,5 @@ int main(void)
 
 
 
-    return 0;
+	return 0;
 }
