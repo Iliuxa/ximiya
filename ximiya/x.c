e0c67bc17f0737c321ad49917004867df17521a7
Олег <oleg.oj17@yandex.ru>
2024-04-07 04:09:36 +0300

    пол большой функции

--- ximiya/x.c
+++ ximiya/x.c
@@ -55,19 +55,19 @@ int main(void)
 	if (mode[4] > 0) {
 		int x = 0;
 
-		if (mode[3] = 0 && mode[2] == 0) {
+		if (mode[3] == 0 && mode[2] == 0) {
 			x = 1;
 		}
 
-		if (mode[3] = 0 && mode[2] == 1) {
+		if (mode[3] == 0 && mode[2] == 1) {
 			x = 2;
 		}
 
-		if (mode[3] = 1 && mode[2] == 0) {
+		if (mode[3] == 1 && mode[2] == 0) {
 			x = 3;
 		}
 
-		if (mode[3] = 01 && mode[2] == 1) {
+		if (mode[3] == 01 && mode[2] == 1) {
 			x = 4;
 		}
 
@@ -77,9 +77,20 @@ int main(void)
 		else {
 			output_1(x);
 		}
-
-
 	}
+
+	if (mode[1] == 1) {
+		printf("TANDEM for NH3");
+	}
+
+	float var_8[8];
+	for (int i = 0; i < 8; i++) {
+		var_8[i] = 0;
+	}
+
+	printf("FEED GAS ");
+
+	output_ch(0, di, var_ch_2, var_8);
 
 
 
@@ -285,4 +296,65 @@ void output_2(int mode)
 		printf("                      L---------<-----------                    ");
 		break;
 	}
+}
+
+void output_ch(int mode, int di, float var_2[8], float var_ch[49], float v_a[8])
+{
+	if (mode == 1) {
+		printf("Q  = %.4f kkal/h;", v_a[5]);
+	}
+
+	if (mode != 1 && mode != 2) {
+
+		if (v_a[0] != 0) {
+			printf("T inlet  = %.1f  *C ;", v_a[0]);
+		}
+
+		if (v_a[6] != 0) {
+			printf("V dry bypass gas = %.2f  m3/h;", v_a[6]);
+		}
+
+		if (v_a[7] != 0) {
+			printf("V bypass steam = %.2f  m3/h ;", v_a[7]);
+		}
+
+		if (v_a[4] != 0) {
+			printf("T outlet from tandem = %.1f *C ;", v_a[5]);
+		}
+	}
+
+	if (var_2[1] != 0) {
+		printf("steam / gas = %.4f;", var_2[1]);
+	}
+
+	if (var_ch[47] != 0) {
+		printf("T = %.1f  *C ;", var_ch[47]);
+	}
+
+	if (var_ch[48] != 0) {
+		printf("P = %.2f  ati;", var_ch[48]);
+	}
+
+	printf("V dry gas  = %.2f  m3/h;", var_2[0]);
+
+	float b[6];
+	for (int i = 0; i < 6; i++) {
+		b[i] = var_2[i + 2] / (var_2[1] + 1);
+	}
+
+	float x1 = (var_2[1] / (var_2[1] + 1)) * 100;
+
+	float b1[49];
+	for (int i = 0; i < 49; i++) {
+		b1[i] = var_ch[i] / (var_2[1] + 1);
+	}
+
+	printf("ã==========================================================¬");
+	printf("¦ compo- ¦                                                 ¦");
+	printf("¦ nents  ¦  moist gas %%       dry gas %%           m3/h     ¦");
+	printf("¦==========================================================¦");
+
+
+
+
 }
